30de7c705bb5606c9e174d0b607780848fdc676a
alhinator <alex@leghart.org>
2024-11-10 15:34:31 -0800

    start scene basic UI & mechanism update

--- Assets/Scripts/Machinery/Mechanism.cs
+++ Assets/Scripts/Machinery/Mechanism.cs
@@ -1,25 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Mechanism : MonoBehaviour
 {
     private bool isPowered;
+    private Animator animator;
     [SerializeField] bool PlayOnAwake = false;
+    public bool repeats = false;
+    public bool bounceMode = false;
+    public bool hasExitAnim = false;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+        if (!animator) { throw new Exception("Mechanism was not provided with an animator!"); }
+        animator.Play("OFF");
         if (PlayOnAwake) { Powered = true; }
     }
 
-    void Update()
-    {
-        if (this.Powered)
-        {
-            PoweredUpdate();
-        }
-    }
 
     public bool Powered
     {
@@ -30,11 +31,29 @@ public class Mechanism : MonoBehaviour
         set
         {
             isPowered = value;
-        }
-    }
+            if (value)
+            {
+                if (bounceMode)
+                {
+                    animator.Play("ON_BOUNCE");
+                    return;
+                }
+                if (repeats)
+                {
+                    animator.Play("ON_REPEAT");
+                }
+                else
+                {
+                    animator.Play("ON_SINGLE");
+                }
+            } else {
+                if (hasExitAnim){
+                    animator.Play("ON_EXITING");
+                } else {
+                    animator.Play("OFF");
+                }
+            }
 
-    private void PoweredUpdate()
-    {
-        throw new NotImplementedException();
+        }
     }
 }
