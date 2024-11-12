3e6bac54860c9af6ee0a1af72d7f895f7bfeaba2
alhinator <alex@leghart.org>
2024-11-11 17:35:53 -0800

    Updated animation to use transitions properly

--- Assets/Scripts/Machinery/Mechanism.cs
+++ Assets/Scripts/Machinery/Mechanism.cs
@@ -7,20 +7,36 @@ using UnityEngine;
 public class Mechanism : MonoBehaviour
 {
     private bool isPowered;
+    private List<GameObject> batteriesInRange;
+    private int currEnergy = 0;
+    [SerializeField] int requiredEnergy;
     private Animator animator;
-    [SerializeField] bool PlayOnAwake = false;
     public bool repeats = false;
-    public bool bounceMode = false;
     public bool hasExitAnim = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        batteriesInRange = new List<GameObject>();
         if (!animator) { throw new Exception("Mechanism was not provided with an animator!"); }
-        animator.Play("OFF");
-        if (PlayOnAwake) { Powered = true; }
+        animator.SetBool("Repeats", repeats);
+        animator.SetBool("HasExitAnim", hasExitAnim);
+        StartCoroutine(PoweredUpdate());
     }
 
+    IEnumerator PoweredUpdate()
+    {
+        currEnergy = Math.Clamp(currEnergy - requiredEnergy, 0, int.MaxValue);
+        foreach (var batt in batteriesInRange)
+        {
+            currEnergy += batt.GetComponent<Battery>().DrawPower(requiredEnergy / batteriesInRange.Count);
+        }
+        Debug.Log(currEnergy);
+        Powered = currEnergy >= requiredEnergy;
+        Debug.Log("poweredupdate: powered  = " + Powered);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(PoweredUpdate());
+    }
 
     public bool Powered
     {
@@ -31,29 +47,23 @@ public class Mechanism : MonoBehaviour
         set
         {
             isPowered = value;
-            if (value)
-            {
-                if (bounceMode)
-                {
-                    animator.Play("ON_BOUNCE");
-                    return;
-                }
-                if (repeats)
-                {
-                    animator.Play("ON_REPEAT");
-                }
-                else
-                {
-                    animator.Play("ON_SINGLE");
-                }
-            } else {
-                if (hasExitAnim){
-                    animator.Play("ON_EXITING");
-                } else {
-                    animator.Play("OFF");
-                }
-            }
-
+            animator.SetBool("Powered", value);
         }
     }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Battery"))
+        {
+            batteriesInRange.Add(col.gameObject);
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Battery"))
+        {
+            batteriesInRange.Remove(col.gameObject);
+        }
+    }
+
 }
