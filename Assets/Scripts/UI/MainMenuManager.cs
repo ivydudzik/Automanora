c84f2ae19a4a3dcd3525d2a33050009b64e98dc1
Ivy Dudzik <ivydudzik@gmail.com>
2024-11-14 18:34:28 -0800

    Add Ferris Wheel Mechanism & Add Battery Mesh

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -9,21 +9,25 @@ public class MainMenuManager : MonoBehaviour
 
     // Menu Container
     VisualElement menuContainer;
-
+    
     private void Start()
     {
+        // Get a reference to the UI container
         menuContainer = menuUIDocument.rootVisualElement.contentContainer;
+        // Register each button to their functionality
         Button startButton = menuContainer.Q<Button>("StartB");
         startButton.RegisterCallback<MouseUpEvent>((evt) => StartGame());
     }
 
     public void StartGame()
     {
+        // Start the scene in the 2nd spot in the build settings list
         SceneManager.LoadScene(1);
     }
 
     public void Quit()
     {
+        // You know what this means
         Application.Quit();
     }
 }
