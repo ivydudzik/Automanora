8f9f737d90d2261ef96f71a317ad7fac78c27f32
alhinator <alex@leghart.org>
2024-11-18 14:13:01 -0800

    start scene + ignore large asset packs

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,28 +1,89 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using System.Collections;
+using UnityEditor.UIElements;
+using System.Threading;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [Header("Menus")]
-    [SerializeField] UIDocument menuUIDocument;
+    UIDocument menuUIDocument;
 
     // Menu Container
     VisualElement menuContainer;
-    
+
     private void Start()
     {
+        menuUIDocument = GetComponent<UIDocument>();
         // Get a reference to the UI container
         menuContainer = menuUIDocument.rootVisualElement.contentContainer;
         // Register each button to their functionality
-        Button startButton = menuContainer.Q<Button>("StartB");
-        startButton.RegisterCallback<MouseUpEvent>((evt) => StartGame());
+
+        SetButtonAnimations();
+        SetButtonFunctions();
+
+    }
+    private void SetButtonAnimations()
+    {
+        menuContainer.Query(className: "button").ForEach((element) =>
+        {
+            element.RegisterCallback<MouseMoveEvent>((evt) =>
+            {
+                //Ref: https://discussions.unity.com/t/quick-transition-tutorial/863467/3
+                element.style.unityBackgroundImageTintColor = Color.cyan;
+                element.style.transitionProperty = new List<StylePropertyName> { new StylePropertyName("unity-background-image-tint-color") };
+                element.style.transitionDuration = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(0.5f, TimeUnit.Second) });
+            });
+            element.RegisterCallback<MouseLeaveEvent>((evt) =>
+            {
+                element.style.unityBackgroundImageTintColor = Color.white;
+                element.style.transitionProperty = new List<StylePropertyName> { new StylePropertyName("unity-background-image-tint-color") };
+                element.style.transitionDuration = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(0.2f, TimeUnit.Second) });
+            });
+        });
+
+    }
+    private void SetButtonFunctions()
+    {
+        menuContainer.Q<Button>("StartB").RegisterCallback<MouseUpEvent>((evt) => StartCoroutine(StartGame()));
+        menuContainer.Q<VisualElement>("FadeBlocker").style.visibility = Visibility.Hidden;
+
+
     }
 
-    public void StartGame()
+    private IEnumerator StartGame()
     {
+        yield return StartCoroutine(FBIn());
         // Start the scene in the 2nd spot in the build settings list
-        SceneManager.LoadScene(1);
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(1);
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator FBIn()
+    {
+        VisualElement FadeBlocker = menuContainer.Q<VisualElement>("FadeBlocker");
+        menuContainer.Q<VisualElement>("FadeBlocker").style.visibility = Visibility.Visible;
+        Debug.Log(FadeBlocker.style.backgroundColor.value.a);
+        while (FadeBlocker.style.backgroundColor.value.a < 1)
+        {
+            FadeBlocker.style.backgroundColor = new Color(0, 0, 0, FadeBlocker.style.backgroundColor.value.a + 0.005f);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    private IEnumerator FBOut()
+    {
+        VisualElement FadeBlocker = menuContainer.Q<VisualElement>("FadeBlocker");
+        while (FadeBlocker.style.backgroundColor.value.a > 0f)
+        {
+            FadeBlocker.style.backgroundColor = new Color(0, 0, 0, FadeBlocker.style.backgroundColor.value.a - 0.005f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        menuContainer.Q<VisualElement>("FadeBlocker").style.visibility = Visibility.Hidden;
     }
 
     public void Quit()
